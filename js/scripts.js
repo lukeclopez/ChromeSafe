24f03a91ec6930a1c95a75d5395d3386e37db66a
Luke Lopez <xlucas14@gmail.com>
2019-12-17 16:51:50 -0500

    If website title or username is empty, don't concat

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ concatTitleAndUsername = () => {
   const websiteTitle = document.getElementById("title").value;
   const username = document.getElementById("username").value;
 
+  if (!websiteTitle || !username) return;
+
   const result = websiteTitle + "|" + username;
 
   document.getElementById("username").value = result;
