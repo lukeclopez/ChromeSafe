855bd6fa0f00648ec4206b9620f29a6f35a2fc33
Yousef Shanawany <askyous@gmail.com>
2019-12-21 19:53:38 -0800

    Fixed issue where refocusing causes title to appear twice

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
-concatTitleAndUsername = () => {
+const concatTitleAndUsername = () => {
+  const delimeter = "|";
   const websiteTitle = document.getElementById("title").value;
   const username = document.getElementById("username").value;
 
-  if (!websiteTitle || !username) return;
+  /** 
+   * Whether the username already has a title in it 
+   * @type {string}
+   */
+  const titleAlreadyIncluded = username.startsWith(websiteTitle + delimeter);
 
-  const result = websiteTitle + "|" + username;
+  if (!websiteTitle || !username || titleAlreadyIncluded) return;
+
+  const result = websiteTitle + delimeter + username;
 
   document.getElementById("username").value = result;
 };
